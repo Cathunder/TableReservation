e38d613cb896ea350f21dab7c3e2d9339c86ab86
takealook <macaroonv@gmail.com>
2024-05-30 16:33:34 +0900

    @EnableJpaAuditing config로 이동

--- src/main/java/com/example/tablereservation/TableReservationApplication.java
+++ src/main/java/com/example/tablereservation/TableReservationApplication.java
@@ -2,10 +2,8 @@ package com.example.tablereservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class TableReservationApplication {
 
     public static void main(String[] args) {
