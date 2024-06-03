e11ce8eede8dd87af990e3296847737553188e58
takealook <macaroonv@gmail.com>
2024-06-04 04:00:04 +0900

    예약등록 관련 dto 추가

---
+++ src/main/java/com/example/tablereservation/reservation/dto/RegisterReservationDto.java
@@ -0,0 +1,53 @@
+package com.example.tablereservation.reservation.dto;
+
+import com.example.tablereservation.common.type.ReservationStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class RegisterReservationDto {
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+        private Long storeId;
+
+        private String loginId;
+        private Integer people;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate date;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        private LocalTime time;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private Long storeId;
+        private String username;
+        private Integer people;
+        private ReservationStatus status;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+        private LocalDateTime reservationDate;
+
+        public static Response fromDto(ReservationDto reservationDto) {
+            return Response.builder()
+                    .storeId(reservationDto.getStoreId())
+                    .username(reservationDto.getUsername())
+                    .people(reservationDto.getPeople())
+                    .status(reservationDto.getStatus())
+                    .reservationDate(reservationDto.getReservationDate())
+                    .build();
+        }
+    }
+}
---
+++ src/main/java/com/example/tablereservation/reservation/dto/ReservationDto.java
@@ -0,0 +1,35 @@
+package com.example.tablereservation.reservation.dto;
+
+import com.example.tablereservation.common.type.ReservationStatus;
+import com.example.tablereservation.reservation.entity.ReservationEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReservationDto {
+    private Long id;
+    private Long storeId;
+    private String username;
+    private Integer people;
+    private ReservationStatus status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime reservationDate;
+
+    public static ReservationDto fromEntity(ReservationEntity reservationEntity) {
+        return ReservationDto.builder()
+                .id(reservationEntity.getId())
+                .storeId(reservationEntity.getStore().getId())
+                .username(reservationEntity.getUsername())
+                .people(reservationEntity.getPeople())
+                .status(reservationEntity.getStatus())
+                .reservationDate(reservationEntity.getReservationDate())
+                .build();
+    }
+}
